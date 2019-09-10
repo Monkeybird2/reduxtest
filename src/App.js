e3c71c8cb17abeec5e67775b2f05894225c14993
michail <refs1@yandex.ru>
2019-09-11 02:46:40 +0300

    first commit2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState = {count: 0};
+
+function reducer(state = {count: 0},action){
+  switch (action.type){
+    case 'INCREMENT': return {count: state.count + action.amount};
+    case 'DECREMENT': return {count: state.count - action.amount};
+    case 'RESET': return {count: 0};
+    default: return state;
+  }
 }
 
-export default App;
+
+
+function increment(amount){
+  return {type: "INCREMENT",amount};
+}
+
+function decrement(amount){
+  return {type: "DECREMENT",amount};
+}
+
+function reset(amount){
+  return  { type: 'RESET'};
+
+}
+const store = createStore(reducer,initialState);
+
+export default class Counter extends Component {
+
+  state = {
+    count: 0
+  }
+  componentDidMount(){
+    store.subscribe(()=> this.forceUpdate())
+  }
+  reset = () => {
+    store.dispatch(reset());
+  }
+  increment =()=>{
+    let amount = parseInt(this.refs.amount.value || 1)
+    store.dispatch(increment(amount));
+  }
+  decrement=()=>{
+    let amount = parseInt(this.refs.amount.value || 1)
+    store.dispatch(decrement(amount));
+  }
+
+  render(){
+    const count = store.getState().count
+    
+    return(
+      <div className="counter">
+        <span className="count">{count}</span>
+        <div className="buttons">
+            <button className="decriment" onClick={this.decrement}>-</button>
+            <button className="reset" onClick={this.reset}>reset</button>
+            <button className="incriment" onClick={this.increment}>+</button>
+         
+          </div>  
+          <input type="text" ref="amount" defaultValue="1"/>
+      </div>
+    );
+  }
+
+
+}
+
